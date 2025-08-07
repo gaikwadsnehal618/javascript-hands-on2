d8969003e1d5944c48550515a2c260f4c7adf6c0
gaikwadsnehal618 <gaikwadsnehal528@gmail.com>
2025-08-07 15:14:03 +0530

    Describe switch
    Describe switch

---
+++ 06-for-assignment.js
@@ -0,0 +1,38 @@
+for (let i = 5; i<=15; i++) {
+    console.log(i);
+    
+}
+console.log("======================");
+for (let i=50; i>=40; i--) {
+    console.log(i);
+    
+}
+console.log("=====================")
+let count = 0;
+let num = 1;
+while (count < 15){
+    console.log(num);
+    num += 2;
+    count++;
+}
+let evenCount = 0;
+let evenNum = 2;
+while (count < 10){
+    console.log(num);
+    num += 2;
+    count++;
+}
+console.log("===================");
+for(let i=1;i<=10;i++){
+    console.log(i*5);
+}
+console.log("==================");
+for(let i=1;i<=10;i++){
+    console.log(i*10);
+}
+console.log("============");
+for(let i=10;i>=1;i--){
+    console.log(i*10);
+}
+
+

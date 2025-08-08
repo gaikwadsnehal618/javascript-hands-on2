64bf51b1c067bcd02c8d44a484bb2d7ad551e7aa
gaikwadsnehal618 <gaikwadsnehal528@gmail.com>
2025-08-08 19:11:26 +0530

    Describe while
    Describe while

---
+++ 06-jump-statment.js
@@ -0,0 +1,14 @@
+for (let index = 0; index <=3; index++) {
+    if(index==1){
+        continue;
+    }
+    console.log(index);
+}
+for (let index = 1; index <=5; index++) {
+    if(index%2==0){
+        continue;
+    }
+    console.log(index);
+    
+    
+}
---
+++ 06-logical-operator.js
@@ -0,0 +1,9 @@
+console.log("=============Reverse string================");
+var str="My Love";
+var Reverse="";
+for (let index = String.length-1; index >=0; index--) {
+    Reverse=Reverse+str.charAt(index);
+}
+console.log(Reverse);
+    
+    

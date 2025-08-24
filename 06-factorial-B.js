f8d60642b1306a825486517cb3ee565996dc7827
gaikwadsnehal618 <gaikwadsnehal528@gmail.com>
2025-08-24 15:09:50 +0530

    Describe fact
    Describe fact

--- 06-factorial-B.js
+++ 06-factorial-B.js
@@ -1,28 +1,28 @@
 function factorialofWordsCount(sentence) {
-    // Unhappy scenario check
+    
     if (typeof sentence !== "string" || sentence.trim() === "" || !isNaN(sentence)) {
         console.log(`Invalid input: ${sentence}`);
         return;
     }
 
-    // Count words
+    
     let words = sentence.trim().split(/\s+/);
     let wordCount = words.length;
 
-    // Factorial function
+    
     function factorial(n) {
         if (n === 0 || n === 1) return 1;
-        return n * factorial(n - 1); // recursive
+        return n * factorial(n - 1); 
     }
 
-    // Output
+    
     console.log(`Sentence: "${sentence}"`);
     console.log(`Word Count: ${wordCount}`);
     console.log(`Factorial of Word Count: ${factorial(wordCount)}`);
     console.log("----------------------------------------");
 }
 
-// Test cases
+
 factorialofWordsCount("Revision is the mother of Success");
 factorialofWordsCount("We never fail, we always learn, Mind it");
 factorialofWordsCount(null);
---
+++ 09-array.js
@@ -0,0 +1,26 @@
+console.log("===========Adding or inserting element in the begining============");
+let arrayElements=[2,1,4,5,9,6];
+console.log("==========Selecting elements from an array===========");
+let arrayNum=[2,1,4,5,9,6,10,70,80,55];
+
+
+console.log("========splice()-To insert the element=======");
+let numberArrays=[2,1,4,5,9,6];
+const numberArrays0=numberArrays.splice(3,0,99,77);
+console.log("========orignal Array=======");
+console.log(numberArrays);
+console.log("=====Inserting an element without replacing or existing=======");
+console.log(numberArrays0);
+console.log("=====for of loop======");
+let numberArr=[2,1,4,5,9,6];
+for(const key in object);
+
+
+
+
+
+
+
+
+
+
